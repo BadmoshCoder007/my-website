c42e49355489692ee93ca212b2d2325bf89165ee
BadmoshCoder007 <singharyan9621695495@gmail.com>
2025-05-26 10:34:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
-body {
-  font-family: Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-  line-height: 1.6;
-}
+console.log("Website loaded!");
 
-header, footer {
-  background-color: #333;
-  color: white;
-  text-align: center;
-  padding: 1em;
-}
-
-nav a {
-  color: #fff;
-  text-decoration: none;
-  margin: 0 10px;
-}
-
-main {
-  padding: 20px;
-}
-@media (max-width: 600px) {
-  nav a {
-    display: block;
-    margin: 5px 0;
-  }
-}
+document.addEventListener("DOMContentLoaded", function () {
+  alert("Welcome to My Website!");
+});
